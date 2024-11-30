7bcc83850d50f76e024ddb310fbc93e500f1da74
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2024-11-30 21:27:16 +0500

    get res on port

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const express = require('express')
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('you are now logged in')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
