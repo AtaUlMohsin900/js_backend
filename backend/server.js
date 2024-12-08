9388d9633cbb0a126c5924e75072f055fa23eda0
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2024-12-08 21:39:24 +0500

    new

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("server is ready")
+    res.send("<h1>Server is Running</h1>");
 });
+app.get('/login', (req, res) => {
+    res.send("This is login page!")
+})
 
 const port = process.env.PORT || 3000
 app.listen(port,() =>{
-    console.log(`server at http://localhost:${prot}`);
-    
+    console.log(`Server at http://localhost:${port}`);
+
+     
 })  
