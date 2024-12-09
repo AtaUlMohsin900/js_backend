7914737cd601ea00acd901744ad01071c135bded
AtaUlMohsin900 <ataulmohisn900@gmail.com>
2024-12-09 08:02:36 +0500

    work in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [books, setBooks] = useState([])
 
   return (
     <>
       <h1>Backend Course</h1>
+      <p>BOOKS:{books.length}</p>
+      {
+        books.map((book, index) => {
+          <div key={book.id}>
+            <h3>{book.title}</h3>
+            <p>{book.auther}</p>
+          </div>
+        })
+      }
     </>
   )
 }
