8aff8baee04b8cce580923a461e77a94d07e586a
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-12 23:44:27 +0000

    update

--- backend/models/todo/sub_todo.models.js
+++ backend/models/todo/sub_todo.models.js
@@ -1,5 +1,27 @@
 import mongoose, { Schema } from "mongoose"
 
-const User = new mongoose.Schema({})
+const User = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+          },
+          email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+          },
+          password: {
+            type: String,
+            required: [true, 'password is required'],
+            unique: true,
+            lowercase: true,
+          },
+
+}
+)
 
 export User = mongoose.model("User", userSchema)
