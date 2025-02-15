947b4f41cf3765f6b13de5d49dcee59978c18350
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-15 18:24:33 +0000

    update

--- backend/models/ecommerce/user.models.js
+++ backend/models/ecommerce/user.models.js
@@ -4,8 +4,20 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
     }
-    
+
 },{timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
